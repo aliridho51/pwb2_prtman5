c54d352366c829a5f20af5fcf43c09570fa52b06
aliridho51 <tongok151@gmail.com>
2024-03-24 22:32:35 +0700

    create layout with templating file

---
+++ ci4 (1)/ci4/app/Views/page/about.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h5>Saya Rifqy Ali Ridho saya dari Rembang jawa tengah saya adalah seorang
+                 pengangguran dan banyak acara
+            </h5>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
---
+++ ci4 (1)/ci4/app/Views/page/home.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h4>Selamat Datang diwebsite kami</h4>
+    </div>
+  </div>
+</div>
+<?= $this->endSection(); ?>
